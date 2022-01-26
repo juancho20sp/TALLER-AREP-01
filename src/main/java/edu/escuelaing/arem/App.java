b68e217d881bd069d58d798d76cf7b8482303ee4
unknown <juan.murillo-g@labinfo.is.escuelaing.edu.co>
2022-01-26 15:20:49 -0500

    AREP-01: Ready for heroku

--- src/main/java/edu/escuelaing/arem/App.java
+++ src/main/java/edu/escuelaing/arem/App.java
@@ -1,5 +1,5 @@
 package edu.escuelaing.arem;
-
+import static spark.Spark.*;
 /**
  * Hello world!
  *
@@ -9,5 +9,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        port(getPort());
+        get("/hello", (req, res) -> "Hello Heroku");
+        get("/hola", (req, res) -> "Hola Heroku");
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set
     }
 }
